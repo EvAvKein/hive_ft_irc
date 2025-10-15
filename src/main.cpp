a555f145d4a47db36349e22210000f8d56849965
datagore <104622881+datagore@users.noreply.github.com>
2025-10-15 21:39:00 +0300

    Add parsing of command line arguments

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,37 @@
 #include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <errno.h>
 
-int main()
+#define PORT_MAX 65535
+
+void start(int port, const char* password)
 {
-	printf("hello world\n");
+	printf("port: %d\n", port);
+	printf("password: '%s'\n", password);
+}
+
+int main(int argc, char** argv)
+{
+	// Check that two arguments were given.
+	if (argc != 3) {
+		printf("usage: ./ircserv <port> <password>\n");
+		return EXIT_FAILURE;
+	}
+
+	// Convert the port to an integer.
+	char* end = nullptr;
+	long port = strtol(argv[1], &end, 10);
+
+	// Check that it's in the valid range of port numbers.
+	if (errno == ERANGE || port < 0 || port > PORT_MAX) {
+		printf("error: port number must be in [0, %d]\n", PORT_MAX);
+		return EXIT_FAILURE;
+	
+	// Check that the whole string was used.
+	} else if (strlen(argv[1]) == 0 || end != argv[1] + strlen(argv[1])) {
+		printf("error: invalid port number '%s'\n", argv[1]);
+		return EXIT_FAILURE;
+	}
+	start(port, argv[2]);
 }
